019b39e3611ac5ed76111000ebaf38939bc3bd89
Hg Q <hgq287@gmail.com>
2025-06-02 15:18:34 +0700

    feat: embed mitmdump in the app, so there's no need to install it via Homebrew

--- Shared/Sources/NetInterceptor/ProxyMan.swift
+++ Shared/Sources/NetInterceptor/ProxyMan.swift
@@ -41,9 +41,12 @@ public class ProxyMan: ObservableObject {
             tempScriptURL = scriptURL
 
             let pipe = Pipe()
-            let mitmPath = "/opt/homebrew/bin/mitmdump" // update path if different
+            let mitmproxyPath = Bundle.main.path(forResource: "mitmproxy.app", ofType: nil)
+            let mitmdumpPath = (
+                mitmproxyPath != nil
+            ) ?  mitmproxyPath! + "/Contents/MacOS/mitmdump" : "/opt/homebrew/bin/mitmdump"
             task = Process()
-            task?.executableURL = URL(fileURLWithPath: mitmPath)
+            task?.executableURL = URL(fileURLWithPath: mitmdumpPath)
             task?.arguments = ["-s", scriptURL.path, " --mode regular", "--listen-port", "8080"]
             task?.standardOutput = pipe
             task?.standardError = pipe
